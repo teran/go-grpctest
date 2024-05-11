6affdcb3ac250a0aeea5bf1fe3283c9e36b91caf
Igor Shishkin <me@teran.dev>
2024-05-12 00:40:04 +0300

    Add autogeneration for protobuf files
    Signed-off-by: Igor Shishkin <me@teran.dev>

--- proto/testservice.go
+++ proto/testservice.go
@@ -1,5 +1,7 @@
 package proto
 
+//go:generate protoc -I=. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative testservice.proto
+
 func NewTestRequest(s string) *TestRequest {
 	return &TestRequest{
 		Message: s,
